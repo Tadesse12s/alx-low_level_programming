3db84ee733ffc5db716137dd665ac4cf99210caf
tadesse12s <tsiyamregn7@gmail.com>
2022-06-03 06:21:29 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio. h>
+int write (int filedes, const char * buf, unsigned int nbyte);
+int main (int argc, char ** argv)
+{
+	write (1, "Hello World! \ n", 13);
+	return 0;
+}#include <stdio. h>
+int write (int filedes, const char * buf, unsigned int nbyte);
+int main (int argc, char ** argv)
+{
+	write (1, "and that piece of art is useful" - Dora Korpar, 2015-10-1 \ n", 13);
+	return 0;
+}
