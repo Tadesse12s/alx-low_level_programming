3b4487b032f3a65b7ca0012a1dc819dbed80d262
tadesse12s <tsiyamregn7@gmail.com>
2022-06-03 13:29:16 -0700

    last commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * this is task 2
+ * this contain all code
+ */
+int main(void)
+{
+	char l;
+
+	for (l = a ; a <= z ; l++)
+	{
+		putchar ("%d", " \n" : l);
+	}
+	return (0);
+}
