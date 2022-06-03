5f6b97da9dcd724193c50f333de8f1f92c76a55b
tadesse12s <tsiyamregn7@gmail.com>
2022-06-03 06:32:31 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio. h>
-int write (int filedes, const char * buf, unsigned int nbyte);
-int main (int argc, char ** argv)
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return:1
+ */
+int main(void)
 {
-	write (1, "Hello World! \ n", 13);
-	return 0;
-}#include <stdio. h>
-int write (int filedes, const char * buf, unsigned int nbyte);
-int main (int argc, char ** argv)
-{
-	write (1, "and that piece of art is useful" - Dora Korpar, 2015-10-1 \ n", 13);
-	return 0;
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\";
+        write(2, ch, sizeof(ch));
+	return (1);
 }
+
