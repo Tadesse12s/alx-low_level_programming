9dae8d997a8bfddd00342ec9d4cb0e17c8fefee4
tadesse12s <tsiyamregn7@gmail.com>
2022-06-06 13:02:01 -0700

    last commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+
+{
+
+	int x ;
+	for ( x = 0 x < 10 )
+	{
+		 putchar( x );
+	}
+	putchar(',');
+	return (0);
+
+}
