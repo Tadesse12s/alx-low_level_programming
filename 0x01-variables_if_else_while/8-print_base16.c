f396731c93bc887200da8696bcab217bf2935eb9
tadesse12s <tsiyamregn7@gmail.com>
2022-06-06 13:13:55 -0700

    combinations of single-digit numbers.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- * Return:0
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
  */
 
 int main(void)
-
 {
+	int c = 0;
 
-	int i, y;
-
-	for (i = 0; i < 10; i++)
+	while (c < 10)
 	{
-	putchar (i + '0');
+	putchar(48 + c);
+	if (c != 9)
+	{
+	putchar(',');
+	putchar(' ');
 	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-	putchar (y);
+	c++;
 	}
 	putchar('\n');
 	return (0);
