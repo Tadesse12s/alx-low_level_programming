2c6f645f1e828ce11d14c38686783616eb539660
tadesse12s <tsiyamregn7@gmail.com>
2022-06-07 12:05:44 -0700

    last commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z')
+	{
+		_putchar(t);
+		_putchar('\n');
+	}
+	return (0);
+}
