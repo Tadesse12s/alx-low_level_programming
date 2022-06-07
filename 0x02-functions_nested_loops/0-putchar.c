12a1080e2b1a6ed4e4db9cbf0c7f3bc43bb6ef9a
tadesse12s <tsiyamregn7@gmail.com>
2022-06-07 10:57:59 -0700

    last commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 int main
 {
-<<<<<<< HEAD
-	        printf(_putchar);
-		        putchar('\n');
-			        return 0;
-=======
-	printf(_putchar);
+	printf('_putchar');
 	putchar('\n');
-	return 0;
->>>>>>> 6215bdbcfc1327882d86dadbf9f2a5e4a6a0e7f9
+	return (0);
 }
