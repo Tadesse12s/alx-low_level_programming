f4519287d21d91b0bcd2c68dd5c0833e9c8ff8ea
tadesse12s <tsiyamregn7@gmail.com>
2022-06-11 17:04:48 +0300

    hecks for a digit (0 through 9).

--- 1-isdigit.c
+++ 1-isdigit.c
@@ -1,10 +1,17 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _isdigit - Entry Point
- * @c : the checked character
- * Return:1 0r 0
+ * _isdigit - Checks for numbers
+ *
+ * @c: character to checked
+ *
+ * Return: 1 OR 0
  */
+
 int _isdigit(int c)
 {
-		return (((c > 47 && c < 58)) ? 1 : 0);
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }
